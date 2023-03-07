3192b3a188a6bcb471dfb7864b0081935ae270e0
Vasiliy <pilaroiding@gmail.com>
2023-03-07 16:53:21 +0300

    Initial commit

---
+++ com/coding/hw_spring_firsttime/HwSpringFirstTimeApplication.java
@@ -0,0 +1,13 @@
+package com.coding.hw_spring_firsttime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSpringFirstTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwSpringFirstTimeApplication.class, args);
+    }
+
+}
